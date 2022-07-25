20bd9863b8f264d7c9d91f1cc10cde8f34f5b262
together08 <together08@yeah.net>
2022-07-25 09:22:58 +0800

    A001D: Implement DT2W node
    Co-authored-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,12 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
