661dca661b063d78d3b748ad78b7a84e46f72e96
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-10-05 16:47:26 -0300

    Update revengeos_A001D.mk

--- revengeos_A001D.mk
+++ revengeos_A001D.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/A001D/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A001D
