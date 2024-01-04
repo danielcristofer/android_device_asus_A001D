387f85c8d34e1d8c7cbc713e4338e71ddc7f5086
Daniel Cristofer <149025121+danielcristofer@users.noreply.github.com>
2024-01-04 09:36:07 -0300

    Update lineage_A001D.mk
    Add TARGET_BOOT_ANIMATION_RES

--- lineage_A001D.mk
+++ lineage_A001D.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from A001D device
 $(call inherit-product, device/asus/A001D/device.mk)
 
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
